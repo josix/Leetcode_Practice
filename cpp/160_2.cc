d68ae0e8a733a16dc3a086fe1905849f1eaf4bb2
Josix <wilson8507@gmail.com>
2019-06-25 22:52:34 +0800

    Create 160_2.cc

---
+++ cpp/160_2.cc
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* first = headA;
+        ListNode* second = headB;
+        while(first != second){
+            first = first ? first -> next:headB;
+            second = second ? second -> next: headA;
+        }
+        return first;
+        
+    }
+};
