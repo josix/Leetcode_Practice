b61c93c7f5a3ed2fa1643685e04553a0443a7dbc
Josix <wilson8507@gmail.com>
2019-07-04 00:09:19 +0800

    Update and rename 71_1.py to 71.py

--- python/71.py
+++ python/71.py
@@ -1,19 +1,14 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        path_list = path.split('/')
-        st = []
+        path_list = [pos for pos in path.split('/') if pos != "" and pos != "."]
+        st = [""]
         while path_list != []:
             pos = path_list.pop(0)
             if st == [] and pos == "":
                 st.append(pos)
-            elif pos == "":
-                pass
-            elif pos == ".." and st[-1] == "":
-                pass
-            elif pos == ".":
-                pass
             elif pos == "..":
-                st.pop()
+                if st[-1] != "":
+                    st.pop()
             else:
                 st.append(pos)
         return "/".join(st) if len(st) > 1 else "/"
