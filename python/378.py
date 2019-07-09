4348fde48fac84eb7426b5d29844f15a28d8b073
Josix <wilson8507@gmail.com>
2019-07-09 20:15:25 +0800

    Update 378.py

--- python/378.py
+++ python/378.py
@@ -1,5 +1,13 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        all_nums = [ele for row in matrix for ele in row]
+        n = len(matrix)
+        candidate = [(row[0], i, 0) for i, row in enumerate(matrix)]
         import heapq as hq
-        return hq.nsmallest(k, all_nums)[-1]
+        count = 0
+        while count < k - 1:
+            _, row_index, col_index = hq.heappop(candidate)
+            count += 1
+            if col_index != n - 1:
+                hq.heappush(candidate, (matrix[row_index][col_index+1], row_index, col_index + 1))
+            
+        return hq.heappop(candidate)[0]
