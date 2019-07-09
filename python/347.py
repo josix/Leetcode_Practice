8d1f075b43f8e5206b02519be38356c7c3ae7278
Josix <wilson8507@gmail.com>
2019-07-09 17:21:10 +0800

    Update 347.py

--- python/347.py
+++ python/347.py
@@ -1,13 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         import heapq as hq
-        num_to_count = {}
-        for num in nums:
-            if num not in num_to_count:
-                num_to_count[num] = 1
-            else:
-                num_to_count[num] += 1
-        nums_with_count = [(count, num) for num, count in num_to_count.items()]
-        hq.heapify(nums_with_count)
-        return [num for _, num in hq.nlargest(k, nums_with_count)]
+        from collections import Counter
+        num_to_count = Counter(nums)
+        return hq.nlargest(k, num_to_count.keys(), key=num_to_count.get)
         
