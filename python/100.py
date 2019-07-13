eb1c81f85c7e291f5414cc15051eddf4dc2a87cf
Josix <wilson8507@gmail.com>
2019-07-13 13:12:55 +0800

    Update 100.py

--- python/100.py
+++ python/100.py
@@ -7,14 +7,26 @@
 
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if p is None and q is None:
-            return True
-        elif p is None and q is not None:
-            return False
-        elif q is None and p is not None:
-            return False
-        elif p.val != q.val:
-            return False
-        elif p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        from collections import deque
+        queue = deque([(p, q)])
+        is_same = None
+        while queue:
+            node_p, node_q = queue.popleft()
+            if node_p is None and node_q is None:
+                is_same = True
+            elif node_p is None or node_q is None:
+                is_same = False
+            elif node_p.val == node_q.val:
+                is_same = True
+            elif node_p.val != node_q.val:
+                is_same = False
+            
+            if is_same and node_p is not None and node_q is not None:
+                queue.append((node_p.left, node_q.left))
+                queue.append((node_p.right, node_q.right))
+            elif is_same and node_p is None and node_q is None:
+                continue
+            else:
+                return False
+        return True
         
